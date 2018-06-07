03230bd07b6b45dcb631c8d9d237ae3ec260c2e2
1n3ffbl3 <mizgier.m@gmail.com>
2018-06-07 13:19:12 +0200

    Refactoring to vue.js

---
+++ Frontend-service/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div id="app">
+    <h3>{{title}}</h3>
+    <div class="col-md-12">
+        <Post 
+        v-for="(id, index) in initial_posts"
+        v-bind:id="id"
+        :key="index"
+        />
+    </div>
+</div>
+</template>
+
+<script>
+import Post from './components/Post.vue'
+export default{
+    name:"app",
+    data(){
+        return{
+            title:"Generate Your Post",
+            initial_posts:[]
+        }
+    },
+    components:{
+        Post
+    }
+}
+</script>
---
+++ Frontend-service/src/components/Post.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="col-md-4" @click="fetchPost">
+    <div class="character-card">
+            <div class="card-block">
+                <h4 class="card-title">{{post.title}}</h4>
+                <p class="card-text">{{post.text}}</p>
+            </div>
+        </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "post",
+  props: ["id"],
+  data() {
+    return {
+      postID: undefined,
+      posts:[],
+      post: {}
+    };
+  },
+  created: function() {
+    this.fetchPost(this.id);
+  },
+  methods: {
+    fetchPost() {
+      fetch("htttp://localhost:3000", {
+        method: "GET"
+      })
+        .then(response => response.json())
+        .then(json => (this.character = json));
+    },
+    getPostById (postID){
+      axios.get('http://localhost:3000/onePost')
+      .then(function(response){
+        if(postID !=undefined){
+          vue.onePost.push(postID)
+          console.log(post);
+        }
+      })
+      .catch(function(error){
+        console.log(error);
+      })
+    }
+  }
+};
+</script>
+
+
